87ac0fad9dc8564ab451d36705f94ecf875e785f
George Wu <georgeqwu@gmail.com>
2013-04-01 20:55:08 -0700

    tried to change patch, but failed lol

--- include/Parser.h
+++ include/Parser.h
@@ -11,16 +11,29 @@ using namespace std;
  */
 
 struct patch {
-	vector<Vector3f> p0;
-	vector<Vector3f> p1;
-	vector<Vector3f> p2;
-	vector<Vector3f> p3;
+	vector<Vector3f> r0;
+	vector<Vector3f> r1;
+	vector<Vector3f> r2;
+	vector<Vector3f> r3;
+	/*
+	vector<Vector3f> c0;
+	vector<Vector3f> c1;
+	vector<Vector3f> c2;
+	vector<Vector3f> c3;
+	*/
+	
 	patch() {
 		vector<Vector3f> a;
-		p0 = a;
-		p1 = a;
-		p2 = a;
-		p3 = a;
+		r0 = a;
+		r1 = a;
+		r2 = a;
+		r3 = a;
+		/*
+		c0 = a;
+		c1 = a;
+		c2 = a;
+		c3 = a;
+		*/
 	}
 	patch(vector<Vector3f> v) {
 		vector<Vector3f> a;
@@ -43,16 +56,41 @@ struct patch {
 		d.push_back(v[13]);
 		d.push_back(v[14]);
 		d.push_back(v[15]);
-		p0 = a;
-		p1 = b;
-		p2 = c;
-		p3 = d;
-	}
-	patch(vector<Vector3f> a, vector<Vector3f> b, vector<Vector3f> c, vector<Vector3f> d) {
-		p0 = a;
-		p1 = b;
-		p2 = c;
-		p3 = d;
+		r0 = a;
+		r1 = b;
+		r2 = c;
+		r3 = d;
+		/*
+		c0 = a;
+		c1 = b;
+		c2 = c;
+		c3 = d;
+		
+		vector<Vector3f> e;
+		vector<Vector3f> f;
+		vector<Vector3f> g;
+		vector<Vector3f> h;
+		e.push_back(v[0]);
+		f.push_back(v[1]);
+		g.push_back(v[2]);
+		h.push_back(v[3]);
+		e.push_back(v[4]);
+		f.push_back(v[5]);
+		g.push_back(v[6]);
+		h.push_back(v[7]);
+		e.push_back(v[8]);
+		f.push_back(v[9]);
+		g.push_back(v[10]);
+		h.push_back(v[11]);
+		e.push_back(v[12]);
+		f.push_back(v[13]);
+		g.push_back(v[14]);
+		h.push_back(v[15]);
+		c0 = e;
+		c1 = f;
+		c2 = g;
+		c3 = h;
+		*/
 	}
 };
 
